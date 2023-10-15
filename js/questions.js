e7ead03e82ae1601d17974f10d187b0f3003c901
TheMayankRohilla <130660526+TheMayankRohilla@users.noreply.github.com>
2023-10-15 18:52:56 +0530

    First Commit

---
+++ js/questions.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What comes next in the sequence: 2, 4, 8, 16, ___?",
+    answer: "32",
+    options: [
+      "24",
+      "32",
+      "64",
+      "48"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If all FLOWS are ROSES, and some ROSES are TULIPS, then which statement is true?",
+    answer: "Some FLOWS are TULIPS.",
+    options: [
+      "All FLOWS are TULIPS.",
+      "Some TULIPS are ROSES.",
+      "Some FLOWS are TULIPS.",
+      "All TULIPS are FLOWS."
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the next letter in the sequence: A, C, E, G, ___?",
+    answer: "I",
+    options: [
+      "H",
+      "I",
+      "J",
+      "K"
+    ]
+  },
+    {
+    numb: 4,
+    question: "If a baseball and a bat cost $110 in total, and the bat costs $100 more than the ball, how much does the ball cost?",
+    answer: "5",
+    options: [
+      "10",
+      "5",
+      "100",
+      "105"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the missing number in this sequence: 2, 5, 10, 17, ___?",
+    answer: "26",
+    options: [
+      "21",
+      "26",
+      "30",
+      "34"
+    ]
+  },
+
+
+    {
+    numb: 6,
+    question: "How many sides does a Dodecagon have?",
+    answer: "12",
+    options: [
+      "12",
+      "22",
+      "11",
+      "10"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "In a race, if you overtake the second person, what position are you in?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "You won the race"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Which of the following is not a prime number?",
+    answer: "21",
+    options: [
+      "13",
+      "17",
+      "21",
+      "23"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "What is the next letter in the sequence: A, B, D, G, ___?",
+    answer: "K",
+    options: [
+      "K",
+      "J",
+      "I",
+      "L"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following is not a primary color?",
+    answer: "Yellow",
+    options: [
+      "Green",
+      "Red",
+      "Yellow",
+      "Blue"
+    ]
+  },
+];
